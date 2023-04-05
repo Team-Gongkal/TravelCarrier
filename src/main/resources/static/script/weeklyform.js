a541f6c57288641a3e977126c2427a76665b0038
CutieYundung <r_-loor@naver.com>
2023-04-05 16:00:31 +0900

    daily/ id 중복오류 수정

---
+++ src/main/resources/static/script/weeklyform.js
@@ -0,0 +1,130 @@
+$(document).ready(function () {
+  $.datepicker.setDefaults($.datepicker.regional["ko"]);
+  $("#sdate").datepicker({
+    // showOn: 'both',
+    // buttonImage: "/", // 버튼 이미지
+    // buttonImageOnly: true, // 버튼에 있는 이미지만 표시한다.
+    changeMonth: true,
+    changeYear: true,
+    nextText: "다음달",
+    prevText: "이전달",
+    dayNames: [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ],
+    dayNamesMin: ["일", "월", "바뀜", "수", "목", "금", "토"],
+    monthNamesShort: [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
+    monthNames: [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
+    dateFormat: "yy-mm-dd",
+    shoMonthAfterYear: true,
+    yearRange: "1960:2023", //연도 범위
+    maxDate: 0, //선택할 수 있는 최소 날짜로 (0: 오늘 이후 날짜 선택 불가하도록 함)
+    onClose: function (selectedDate) {
+      //시작일(startDate) datepicker가 닫힐때
+      //종료일(endDate)의 선택할수있는 최소 날짜(minDate)를 선택한 시작일로 지정
+      $("#edate").datepicker("option", "minDate", selectedDate);
+    },
+  });
+  //end Date
+  $("#edate").datepicker({
+    // showOn: 'both',
+    // buttonImage: "/", // 버튼 이미지
+    // buttonImageOnly: true, // 버튼에 있는 이미지만 표시한다.
+    changeMonth: true,
+    changeYear: true,
+    nextText: "다음달",
+    prevText: "이전달",
+    dayNames: [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ],
+    dayNamesMin: ["일", "월", "바뀜", "수", "목", "금", "토"],
+    monthNamesShort: [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
+    monthNames: [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
+    dateFormat: "yy-mm-dd",
+    shoMonthAfterYear: true,
+    yearRange: "1960:2023", //연도 범위
+    maxDate: 0, //선택할 수 있는 최소 날짜로 (0: 오늘 이후 날짜 선택 불가하도록 함)
+    onClose: function (selectedDate) {
+      //시작일(startDate) datepicker가 닫힐때
+      //종료일(endDate)의 선택할수있는 최소 날짜(minDate)를 선택한 시작일로 지정
+      $("#sdate").datepicker("option", "maxDate", selectedDate);
+    },
+  });
+  //Date Click
+  $(".dateClick").on("click", function (e) {
+    $(e.target).prev("input").focus();
+    console.log(e.target);
+  });
+  //edate Click]
+
+  // 동행인 모달창--------------------------------------
+  $("#plus_companion").on("click", function () {
+    $(".companion_modal_bg").addClass("show");
+  });
+  $(".companion_modal button").on("click", function () {
+    $(".companion_modal_bg").removeClass("show");
+  });
+}); //스크립트 종료
