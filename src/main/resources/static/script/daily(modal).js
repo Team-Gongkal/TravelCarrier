dd2c9e31a6cba98dd6758a67c109bd8025797c3c
cheshireHYUN <seohyun@chungbuk.ac.kr>
2023-04-08 17:32:59 +0900

    데일리폼 - 초기 화면단 변경 js 작성완료

--- src/main/resources/static/script/daily(modal).js
+++ src/main/resources/static/script/daily(modal).js
@@ -1,4 +1,8 @@
-//li id에 main을 넣으면 대표사진, 클릭중인건 li의 class가 clickImg
+
+$(document).ready(function() {
+   //첫번째 탭 자동클릭
+   $('ul.days_tabSlide li:first').click();
+});
 
 //화면 로드되면 바로 click Day1 해주기
 var liIndex = "";
@@ -8,15 +12,13 @@ var dataArr = [];
 var selectArr = [];
 
 // function : 사진첨부시 동작 (by.서현)
-$(document).on('change', '#moreImg', function(event){
+$(document).on('change', '.attach', function(event){
     setDataArr(event);
     getCurrentDataArr();
     drawThumbs(selectArr);
-
-    //처음 로드시엔 맨 처음요소 선택하게 하기..
+    //처음 로드시엔 맨 처음요소 선택하게 하기
     $('ul.Dform_imglist li:first img').click();
 });
-
 // function : 사진첨부시 dataArr 생성 (by.서현)
 function setDataArr(event){
     var formDataArr = selectArr;
@@ -31,6 +33,7 @@ function setDataArr(event){
     var obj = { day : [$('.days_tabSlide .on').text()],
                 data : formDataArr };
     dataArr.push(obj);
+    console.log("추가분 : "+obj.day+", "+obj.data);
 }
 
 // 오른쪽 폼(제목,메모,경로,대표) 비우기
@@ -43,18 +46,20 @@ function removeRightForm(){
 }
 // 사진 클릭이벤트
 $(document).on('click', 'ul.Dform_imglist li img', function(event){
-
     removeRightForm();
 
-    $(this).closest('li').attr('class','clickImg');
-    liIndex = $('ul.Dform_imglist li').index($('li.clickImg'));
-    $('div.daily_title input[type="text"]').val(selectArr[liIndex].get('title'));
-    $('div.daily_text textarea').val(selectArr[liIndex].get('text'));
-    $('div.daily_files p').text(selectArr[liIndex].get('file').name);
-    if(selectArr[liIndex].get('thumb')==0){
-        $('input[type="radio"]').prop('checked', false);
-    } else if(selectArr[liIndex].get('thumb')==1){
-        $('input[type="radio"]').prop('checked', true);
+    //만약 selectArr이 0, 즉 defailt가 떠있으면 실행 X
+    if(selectArr.length !== 0){
+        $(this).closest('li').attr('class','clickImg');
+        liIndex = $('ul.Dform_imglist li').index($('li.clickImg'));
+        $('div.daily_title input[type="text"]').val(selectArr[liIndex].get('title'));
+        $('div.daily_text textarea').val(selectArr[liIndex].get('text'));
+        $('div.daily_files p').text(selectArr[liIndex].get('file').name);
+        if(selectArr[liIndex].get('thumb')==0){
+            $('input[type="radio"]').prop('checked', false);
+        } else if(selectArr[liIndex].get('thumb')==1){
+            $('input[type="radio"]').prop('checked', true);
+        }
     }
 });
 
@@ -103,31 +108,42 @@ function getCurrentDataArr(){
     // 클릭한 DAY 정보에 대한 data 셋팅
     var selectDay = $('span.on').text();
     selectArr = dataArr.find(item => item.day[0] === selectDay); //이게 데이터
-    if(selectArr !== undefined) selectArr = selectArr.data;
-    else selectArr = [];
-    console.log("a : "+selectArr);
+    if(selectArr !== undefined){
+        selectArr = selectArr.data;
+    }
+    else {
+        selectArr = [];
+    }
 }
 
 // function : formDataArr를 폼에 띄우기 (by.서현)
 function drawThumbs(selectArr){
     //전체를 그리는 메소드이므로 그리기 전에 이전 데이터 싹 지우기
-    $('.Dform_imglist').empty();
-    for (const formData of selectArr) {
-         //맨뒤에 추가가 아니라 순서대로 화면에 로드만 하면됨
-         var img = $('<img>').attr('src', URL.createObjectURL(formData.get('file')));
-         //main인 데이터 이미 있으면 붙여줘야됨
-         if(formData.get('thumb')==0){
-            var li = $('<li>').append(img);
-         }else if(formData.get('thumb')==1){
-            var li = $('<li>').attr('id', 'main').append(img);
+    if(selectArr.length > 0){
+        $(".default_Dform_imgs").hide();
+        $(".Dform_imgs").show();
+        $('.Dform_imglist').empty();
+        for (const formData of selectArr) {
+             //맨뒤에 추가가 아니라 순서대로 화면에 로드만 하면됨
+             var img = $('<img>').attr('src', URL.createObjectURL(formData.get('file')));
+             //main인 데이터 이미 있으면 붙여줘야됨
+             if(formData.get('thumb')==0){
+                var li = $('<li>').append(img);
+             }else if(formData.get('thumb')==1){
+                var li = $('<li>').attr('id', 'main').append(img);
+             }
+             $('.Dform_imglist').append(li);
          }
-         $('.Dform_imglist').append(li);
-     }
-     //추가버튼도 다시 생성
-    $('.Dform_imglist').append('<li id="moreImgLi" >'+
-               '<input type="file" multiple id="moreImg"></input>'+
-               '<label for="moreImg"><i class="xi-plus"></i></label>'+
-             '</li>');
+         //추가버튼도 다시 생성
+        $('.Dform_imglist').append('<li id="moreImgLi" >'+
+                   '<input type="file" multiple id="moreImg" class="attach"></input>'+
+                   '<label for="moreImg"><i class="xi-plus"></i></label>'+
+                 '</li>');
+    } else{
+        //만약 해당 탭에 아무 데이터가 없다면 default_Dform_imgs를 지워줘야한다.
+        $(".Dform_imgs").hide();
+        $(".default_Dform_imgs").show();
+    }
 }
 
 
