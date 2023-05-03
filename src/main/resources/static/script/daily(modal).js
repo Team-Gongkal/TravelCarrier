f9974490de915759bb508b033463e1d49d5436c3
cheshireHYUN <seohyun@chungbuk.ac.kr>
2023-05-03 17:39:18 +0900

    데일리 ajax url 수정

--- src/main/resources/static/script/daily(modal).js
+++ src/main/resources/static/script/daily(modal).js
@@ -308,10 +308,8 @@ $("ul.Dform_imglist").sortable({
 });
 //url 구해두기
 var currentUrl = window.location.href;
-var extractedUrl = currentUrl.match(
-  /\/TravelCarrier\/weekly\/\d+\/daily(?:[^&]*)?/
-)[0];
-console.log(extractedUrl);
+var weeklyId = currentUrl.match(/weekly\/(\d+)\/daily/)[1];
+
 //저장시 ajax
 // dataArr : [ {DAY1,formDataArr[0]},{DAY2,formDataArr[1]},{DAY3,formDataArr[2]} ]
 // formDataArr : [{formData},{file,title, text,thumb},{file,title, text,thumb}]
@@ -355,8 +353,7 @@ $(document).on("click", "button.Dform_btn_save", function (event) {
 
   alert(extractedUrl + "/create");
   $.ajax({
-    //url: "/TravelCarrier/weekly/83/daily" + "/create",
-    url: extractedUrl + "/create",
+    url: "/TravelCarrier/weekly/"+weeklyId+"/daily" + "/create",
     type: "POST",
     data: postData,
     processData: false,
