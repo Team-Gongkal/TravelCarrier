ce734def96f2fab6fcbbd1fb39845654cdb7d43d
CutieYundung <r_-loor@naver.com>
2023-05-29 06:58:12 +0900

    login / 애니메이션 및 스크롤이벤트 구현중

--- src/main/resources/static/script/login.js
+++ src/main/resources/static/script/login.js
@@ -85,10 +85,72 @@ function validCheck() {
   });
 */
 
+//애니메이션 실행 -by윤아
+$(window).on("load", function () {
+  $(".start_title").addClass("on");
+
+  $(".start_title button").click(function () {
+    $("#starting_page").addClass("hide");
+    $(".example_img_wrap").addClass("show");
+  });
+});
+
 // 로그인 모달창 활성화 및 비활성화 -by윤아
 $(".quick_login").click(function () {
   $("#login_wrap").addClass("show");
 });
 $("#login_wrap > div > button.close").click(function () {
   $("#login_wrap").removeClass("show");
+});
+
+// 원페이지 스크롤 구현 - by 윤아
+let num = 0; //첫페이지에서 스크롤되는 횟수(첫페이지 동영상 크기 변환) -스크롤을 할 때마다 증가하며 그에 따라서 비디오의 크기가 변한다
+let idx = 0; //첫페이지에서 다음페이지로 넘어갈 때부터 카운트 되는 스크롤 횟수(페이지 이동을 하기위한 변수)- num이 일정 숫자에 달하면 (= 비디오가 화면을 꽉 채우게 되는 수치) 증가가 멈추며, idx가 증가하게 된다.
+let delta; //스크롤의 방향을 판별하기 위한 변수로 할당은 스크롤이벤트 안에서 함
+const mainVideo = document.querySelector("#example_main_video video");
+const gally = document.querySelector(".example_img_wrap");
+const lastPage = document.querySelectorAll("#start section").length;
+
+//mousewheel DOMMouseScroll 스크롤 이벤트
+$(window).on("mousewheel DOMMouseScroll", function (e) {
+  delta = e.originalEvent.wheelDelta || e.originalEvent.delta * -1;
+
+  if (delta < 0) {
+    //스크롤을 아래로 내렸을 때
+    if (!(num == 13)) {
+      num++;
+      console.log(num);
+    }
+    if (num <= 12) {
+      mainVideo.style.clipPath = `inset(${23 - num * 2}% ${
+        35 - num * 3
+      }% round 20px)`;
+      gally.style = `grid-template-columns : 1fr 1fr ${num}fr 1fr 1fr`;
+      gally.style.width = `${100 + num * 5}%`;
+      // 비주얼 컨텐츠 영역의 넓이가 변하도록 스타일 조정(커짐)
+    } else if (num == 13 && idx < lastPage) {
+      idx++;
+      console.log("최종" + idx);
+    }
+  } else {
+    // if (idx > 0) {
+    //   idx--;
+    //   console.log("-" + idx);
+    // }
+    if (idx == 1 && num > 0) {
+      num--;
+      mainVideo.style.clipPath = `inset(${23 - num}% ${35 - num}% round 20px)`;
+      gally.style = `grid-template-columns : 1fr 1fr ${num}fr 1fr 1fr`;
+      gally.style.width = `${100 + num * 5}%`;
+      console.log(num);
+    }
+  }
+  $("html,body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(window).height() * (idx - 1),
+      },
+      1000
+    );
 });
