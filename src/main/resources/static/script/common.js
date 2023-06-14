50c5782898b3fc13dffbb41991e0dfd6940e7194
CutieYundung <r_-loor@naver.com>
2023-06-14 15:24:32 +0900

    daily / 슬라이드 수정 완료

--- src/main/resources/static/script/common.js
+++ src/main/resources/static/script/common.js
@@ -1,7 +1,8 @@
 $(window).click(function (e) {
+  console.log($(e.target));
   if ($(e.target).is($(".modal_bg"))) {
     $(
-      ".weekly_modal_bg, .keyword_modal_bg, .edit_profile, #login_wrap , .reply_modal_bg , .daily_form_bg"
+      ".weekly_modal_bg, .keyword_modal_bg, .edit_profile, #login_wrap , .reply_modal , .daily_form_bg"
     ).removeClass("show");
   }
 });
