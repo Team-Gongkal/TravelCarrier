43954a18569c30142c2f3f3ab04614f8902293f0
CutieYundung <r_-loor@naver.com>
2023-08-28 14:54:01 +0900

    comment: alert.js 콘솔 제거 및 주석 정리

--- src/main/resources/static/script/alert.js
+++ src/main/resources/static/script/alert.js
@@ -1,9 +1,26 @@
 // 각종 경고 메시지 및 알림창 스크립트 파일
+
 function alertModal(text) {
-  //클릭한 요소의text가져오기
-  //span을 파라미터로 바꿔주기
   var changeText = $(".alert_textbox p").eq(1).children("span");
+  //span을 파라미터로 바꿔주기
   changeText.text(text);
-  console.log(changeText.text());
+  //모달창 활성화 하기(show)
   $("#alert_modal").addClass("show");
+}
+
+//로그아웃-------------------------------------
+function confirmLogout(event) {
+  event.preventDefault(); // 폼 제출 방지
+  alertModal("로그아웃");
+  //'네'버튼에 logout-id부여
+  $(".confirm_btn").attr("id", "logout");
+}
+
+$(document).on("click", "#logout", function () {
+  document.getElementById("logoutForm").submit(); // 폼 제출
+});
+
+//알림창 닫기====================================
+function closeAlert() {
+  $("#alert_modal").removeClass("show");
 }
