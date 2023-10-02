5dacdf7df7d2966f12156b7fc65319bbb32d6f63
cheshireHYUN <seohyun@chungbuk.ac.kr>
2023-10-02 18:52:10 +0900

    Fix. 위클리 수정모달 기본셋팅값을 오늘날짜 -> 이전데이터로 변경

--- src/main/resources/static/script/weekly.js
+++ src/main/resources/static/script/weekly.js
@@ -440,5 +440,5 @@ function closeUpdateModel(){
 $(document).on("click",".close_and_del .close",function(e){
     if(isChange()){
         weekly_update_close_alert();
-    }
+    }else closeUpdateModel();
 })
